0b53953f168912233703d82af01395aea5fa8b0b
Slime_hatena <slime.hatena@gmail.com>
2015-11-02 02:16:08 +0900

    P名 Twitter PRP 各難易度フルコン状況の描画
    次やること
    masterのみに実装した１桁の場合の処理を他難易度に実装
    もしくは、右揃えにして描画する処理を追加する.

--- process.php
+++ process.php
@@ -42,17 +42,17 @@
     $twitter_x = 33;
     $twitter_y = 251;
 
-    $prp_x = 300;
-    $prp_y = 270;
+    $prp_x = 290;
+    $prp_y = 293;
 
     $debut_x = 555;
-    $debut_y = 200;
+    $debut_y = 221;
     $regular_x = 555;
-    $regular_y = 234;
+    $regular_y = 253;
     $pro_x = 555;
-    $pro_y = 264;
+    $pro_y = 284;
     $master_x = 555;
-    $master_y = 294;
+    $master_y = 314;
     $masplus_x = 555;
     $masplus_y = 324;
 
@@ -78,8 +78,7 @@
     $masplus = null;
 
 
-    //フォントの指定
-    $font = 'font/mplus-2c-regular.ttf';
+
 
 
 
@@ -87,13 +86,29 @@
     $p_rank = 80;
     $name = mb_convert_encoding('秋雨', 'UTF-8', 'auto');
     $twitter = mb_convert_encoding('Slime_hatena', 'UTF-8', 'auto');
-    $prp = 3340;
+    $prp = mb_convert_encoding('3340', 'UTF-8', 'auto');;
     $debut = 33;
     $regular = 22;
     $pro = 11;
     $master = 1;
     $masplus = 0;
     //デバッグ用ここまで---------------------------
+
+    //フルコン数が１桁の時に空白を入れる処理
+    //                                  masterしかつくってない
+
+    if ($master < 10) {
+
+        $master = ' ' . $master;
+
+    }
+
+    //フルコン曲数を 30 / 30みたいにする
+    $r_debut = $debut . ' / ' . $music_max;
+    $r_regular = $regular . ' / ' . $music_max;
+    $r_pro = $pro . ' / ' . $music_max;
+    $r_master = $master . ' / ' . $music_max;
+    $r_masplus = $masplus . ' / ' . $music_max_masplus;
 
     //画像読み込み
     $img = imagecreatefrompng('img/body.png');
@@ -102,14 +117,19 @@
     $black = ImageColorAllocate($img, 0x00, 0x00, 0x00);
     $white = ImageColorAllocate($img, 0xFF, 0xFF, 0xFF);
 
+    //フォントの指定
+    $font = 'font/mplus-2c-regular.ttf';
 
 
 
     //文字の描写
     ImageTTFText($img, 46, 0, $name_x, $name_y, $black,'font/mplus-2c-regular.ttf', $name); //P名
     ImageTTFText($img, 26, 0, $twitter_x, $twitter_y, $black,'font/mplus-2c-regular.ttf', $twitter); //Twitter
-
-
+    ImageTTFText($img, 24, 0, $prp_x, $prp_y, $black,'font/mplus-2c-regular.ttf', $prp); //PRP
+    ImageTTFText($img, 20, 0, $debut_x, $debut_y, $black,'font/mplus-2c-regular.ttf', $r_debut); //Debut
+    ImageTTFText($img, 20, 0, $regular_x, $regular_y, $black,'font/mplus-2c-regular.ttf', $r_regular); //Regular
+    ImageTTFText($img, 20, 0, $pro_x, $pro_y, $black,'font/mplus-2c-regular.ttf', $r_pro); //Pro
+    ImageTTFText($img, 20, 0, $master_x, $master_y, $black,'font/mplus-2c-regular.ttf', $r_master); //Master
 
     // 画像の出力
     header('Content-Type: image/png');
